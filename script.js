13de92c4394b7c12d82e210d4cd922d49a1a37ac
yassine <yassineoudadda027@gmail.com>
2024-10-17 18:50:11 +0100

    setting up the starting game with functions that allows to check win check draw swip turns and mark

---
+++ script.js
@@ -0,0 +1,89 @@
+const X_Class='x';
+const circle_Class='circle';
+const WINNING_COMBINATIONS=[
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+
+]
+const cellElements=document.querySelectorAll('[data-cell]');
+const board=document.getElementById("board")
+const winningMessageTextElement=document.querySelector('[data-winning-message-text]')
+let circleTurn ;
+
+startGame()
+
+
+function startGame(){
+  circleTurn=false;
+  cellElements.forEach(cell=> {
+    cell.addEventListener('click',handleClick,{once:true})
+  })
+  setBoardHoverClass()
+
+}
+
+
+
+
+
+function handleClick(e){
+
+  const cell=e.target;
+  const currentClass=circleTurn ? circle_Class : X_Class ; 
+
+  //place the mark
+  
+  placeMark(cell,currentClass)
+  //check win 
+  if (checkWin(currentClass)){
+    console.log("winner")
+  }
+
+  //check draw
+
+  //switch turns
+  swapTurn();
+  setBoardHoverClass();
+}
+
+function placeMark(cell,currentClass){ 
+  cell.classList.add(currentClass);
+}
+
+function swapTurn(){
+  circleTurn=!circleTurn;
+}
+
+function setBoardHoverClass(){
+  board.classList.remove(X_Class);
+  board.classList.remove(circle_Class);
+  if (circleTurn){
+    board.classList.add(circle_Class);
+  }else{
+    board.classList.add(X_Class)
+  }
+}
+
+function checkWin(currentClass){
+  return WINNING_COMBINATIONS.some(combination => {
+    return combination.every(index=>{
+      return cellElements[index].classList.contains(currentClass)
+    })
+  })
+}
+
+
+function endGame(draw){
+  if (draw){
+
+  }else {
+    winningMessageTextElement
+  }
+
+}
